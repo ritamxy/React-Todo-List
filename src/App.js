14cc23e06c7e6e12304f3c65855cb8ee12cfefa2
ritamxy <ritamxy@gmail.com>
2020-01-17 15:34:11 +1100

    add default function for input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import uuid from "uuid";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
@@ -13,13 +14,31 @@ import {
 library.add(faBook, faTrashAlt, faPencilAlt);
 
 class App extends Component {
+  state = {
+    items: [],
+    id: 0,
+    item: "",
+    editItem: false
+  };
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">Todo input</h3>
-            <TodoInput />
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
             <TodoList />
           </div>
         </div>
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -4,9 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class TodoList extends Component {
   state = {};
   render() {
+    const { item, handleChange, handleSubmit } = this.props;
     return (
       <div className="card card-body my-3">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <div className="input-group-prepend">
               <div className="input-group-text bg-primary text-white ">
@@ -17,6 +18,8 @@ class TodoList extends Component {
               type="text"
               className="form-control text-capitalize"
               placeholder="add a todo item"
+              value={item}
+              onChange={handleChange}
             />
           </div>
           <button type="submit" className="btn btn-block btn-primary mt-3">
